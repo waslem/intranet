754efe517fa0f365116f95eb46c967c34307b17c
waslem <jvw@westnet.com.au>
2015-09-11 19:37:04 +0800

    added main website

---
+++ www.waitservices.com/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="www.waitservices.com.MvcApplication" Language="C#" %>
---
+++ www.waitservices.com/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(www.waitservices.com.Startup))]
+namespace www.waitservices.com
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
