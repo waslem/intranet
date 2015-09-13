07cf11bf4fda7073e215792e297996dd59476c0b
waslem <jvw@westnet.com.au>
2015-09-13 19:54:39 +0800

    Added front end bootstrap theme

--- www.waitservices.com/App_Start/BundleConfig.cs
+++ www.waitservices.com/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace www.waitservices.com
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/main.css", 
+                      "~/Content/font-awesome.min.css"));
         }
     }
 }
